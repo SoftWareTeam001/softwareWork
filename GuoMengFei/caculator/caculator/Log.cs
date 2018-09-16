2a90ca8e32f9ac0fe5a6a90000c46e2b812f2b52
Maker <maker@qq.com>
2018-09-16 12:13:36 +0800

    reactor

--- GuoMengFei/caculator/caculator/Log.cs
+++ GuoMengFei/caculator/caculator/Log.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Windows;
 using System.Text.RegularExpressions;
+//reactor
 namespace caculator
 {
     class Log
--- GuoMengFei/caculator/caculator/UpDirectionButton.cs
+++ GuoMengFei/caculator/caculator/UpDirectionButton.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//reactor
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
